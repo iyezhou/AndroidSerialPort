3bfdcf3b4d23ee03a878a85fc00e3e1d096dbcc3
zengwendi <wendi.zeng@swiftpass.cn>
2020-01-09 17:38:28 +0800

    init

--- SerialPortLibrary/src/main/cpp/SerialPort.h
+++ SerialPortLibrary/src/main/cpp/SerialPort.h
@@ -2,8 +2,8 @@
 #include <jni.h>
 /* Header for class android_serialport_api_SerialPort */
 
-#ifndef _Included_qingwei_kong_serialportlibrary_SerialPort
-#define _Included_qingwei_kong_serialportlibrary_SerialPort
+#ifndef _Included_qingwei_swiftpass_serialportlibrary_SerialPort
+#define _Included_qingwei_swiftpass_serialportlibrary_SerialPort
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -13,7 +13,7 @@ extern "C" {
  * Method:    open
  * Signature: (Ljava/lang/String;II)Ljava/io/FileDescriptor;
  */
-JNIEXPORT jobject JNICALL Java_com_kongqw_serialportlibrary_SerialPort_open
+JNIEXPORT jobject JNICALL Java_com_swiftpass_serialportlibrary_SerialPort_open
   (JNIEnv *, jclass, jstring, jint, jint);
 
 /*
@@ -21,7 +21,7 @@ JNIEXPORT jobject JNICALL Java_com_kongqw_serialportlibrary_SerialPort_open
  * Method:    close
  * Signature: ()V
  */
-JNIEXPORT void JNICALL Java_com_kongqw_serialportlibrary_SerialPort_close
+JNIEXPORT void JNICALL Java_com_swiftpass_serialportlibrary_SerialPort_close
   (JNIEnv *, jobject);
 
 #ifdef __cplusplus
